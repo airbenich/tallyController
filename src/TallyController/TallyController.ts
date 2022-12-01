c633ecc38548848abf80ba5f372c057c314f0b26
Vivian Erbenich <vivian@erbenich.eu>
2022-12-01 21:15:37 +0100

    adds basic canon xc protocol commands 2

--- src/TallyController/TallyController.ts
+++ src/TallyController/TallyController.ts
@@ -3,6 +3,7 @@ import { VideoSwitcher } from './../VideoSwitcher/VideoSwitcher';
 import { TallyLight } from './../TallyLight/TallyLight';
 import { MultiViewTally } from '../MultiViewTally/MultiViewTally';
 import { TallyWebsocketServer } from './../TallyWebsocketServer/TallyWebsocketServer';
+import CanonXCController from './../CanonXC/CanonXCController';
 
 export class TallyController {
   private tallyWebsocketServer = new TallyWebsocketServer({
@@ -36,6 +37,13 @@ export class TallyController {
     identifier: 'mobil',
     type: 'websocket',
     websocketServer: this.tallyWebsocketServer,
+  });
+
+  private mobileCanonCameraTally1 = new CanonXCController({
+    name: 'Mobile Camera 5 – Canon XF605',
+    ipAdress: '172.17.121.85',
+    username: 'test',
+    password: 'test'
   });
 
   constructor() {
@@ -95,30 +103,36 @@ export class TallyController {
     this.panasonicRpController.wasChanged = false;
   }
 
-  private handleWebsocketsStateChanged() {
-    const websocketCameraNumber = 7;
-    // console.log('handleWebsocketsStateChanged:');
-    // console.log({
-    //   inputPreviewState: this.videoSwitcher.inputPreviewState,
-    //   inputProgramState: this.videoSwitcher.inputProgramState,
-    //   inTransition: this.videoSwitcher.inTransition,
-    // });
+  private handleCamsStateChanged() {
+    // camera is on input 7 on multivew but called: cam 5
+    const mobileCam5 = 7;
+    console.log('handleCamsStateChanged:');
+    console.log({
+      inputPreviewState: this.videoSwitcher.inputPreviewState,
+      inputProgramState: this.videoSwitcher.inputProgramState,
+      inTransition: this.videoSwitcher.inTransition,
+    });
 
     // handle Remote Tally
-    if (websocketCameraNumber === this.videoSwitcher.inputProgramState) {
+    if (mobileCam5 === this.videoSwitcher.inputProgramState) {
       if (this.videoSwitcher.inTransition) {
         this.mobileCameraTallyLight1.setInTransitionTally();
+        this.mobileCanonCameraTally1.setInTransitionTally();
       } else {
         this.mobileCameraTallyLight1.setProgramTally();
+        this.mobileCanonCameraTally1.setProgramTally();
       }
-    } else if (websocketCameraNumber === this.videoSwitcher.inputPreviewState) {
+    } else if (mobileCam5 === this.videoSwitcher.inputPreviewState) {
       if (this.videoSwitcher.inTransition) {
         this.mobileCameraTallyLight1.setInTransitionTally();
+        this.mobileCanonCameraTally1.setInTransitionTally();
       } else {
         this.mobileCameraTallyLight1.setPreviewTally();
+        this.mobileCanonCameraTally1.setPreviewTally();
       }
     } else {
       this.mobileCameraTallyLight1.setTallyOff();
+      this.mobileCanonCameraTally1.setTallyOff();
     }
   }
 
@@ -151,21 +165,21 @@ export class TallyController {
     console.log('tallyPreviewChange');
 
     this.handleRemoteTallyStateChange();
-    this.handleWebsocketsStateChanged();
+    this.handleCamsStateChanged();
     this.handleTallyChangeForMultiview();
   }
   public tallyProgramChange() {
     console.log('tallyProgramChange');
 
     this.handleRemoteTallyStateChange();
-    this.handleWebsocketsStateChanged();
+    this.handleCamsStateChanged();
     this.handleTallyChangeForMultiview();
   }
 
   public tallyInTransitionChange() {
     console.log('tallyInTransitionChange:' + this.videoSwitcher.inTransition);
     this.handleRemoteTallyStateChange();
-    this.handleWebsocketsStateChanged();
+    this.handleCamsStateChanged();
     this.handleTallyChangeForMultiview();
   }
 }
